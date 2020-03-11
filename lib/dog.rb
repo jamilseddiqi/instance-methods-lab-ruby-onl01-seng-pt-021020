edd7d791aab3c12688a4c28a51f1d8141500fe0b
Jamil Seddiqi <jamilsediq@gmail.com>
2020-03-11 12:48:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,49 @@
-class Dog
-  def bark  # define our instance method
-    puts 'Woof!'
-  end
+require 'pry'
+
+class Ball
+  # attr_accessor :color, :size, :brand 
+  attr_writer :color, :size, :brand 
+  # attr_reader :color, :size, :brand 
   
-  def sit 
-    puts "The Dog is sitting"
-  end 
+def initialize(color, size, brand)
+  @color = color 
+  @size = size
+  @brand = brand 
+end 
+
+  
+  
+  # def study=(programming)
+  #   @study = programming 
+  # end 
+  
+  # def training=(content)
+  #   @training = content
+  # end 
+  # def study 
+  #   @study = programming
+  # end 
+  # def training
+  #   @training = training
+  # end 
+  
 end
 
-fido = Dog.new
-fido.bark  
-f = Dog.new
-f.sit 
+binding.pry 
+
+
+
+# class Dog
+#   def bark  # define our instance method
+#     puts 'Woof!'
+#   end
+  
+#   def sit 
+#     puts "The Dog is sitting"
+#   end 
+# end
+
+# fido = Dog.new
+# fido.bark  
+# f = Dog.new
+# f.sit 
